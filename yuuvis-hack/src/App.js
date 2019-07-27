4b55a6afbae63939116d01cf7a1e931a4e71bf39
Rachit Pareek <rachitpareek@Rachits-MacBook-Pro.local>
2019-07-27 15:46:07 -0700

    rachit's commit

--- yuuvis-hack/src/App.js
+++ yuuvis-hack/src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import HomePage from './components/pages/HomePage';
 
 function App() {
   return (
