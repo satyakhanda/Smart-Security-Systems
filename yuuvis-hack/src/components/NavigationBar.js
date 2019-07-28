36ec1585b0ab42359630adac922843a173e360af
Rachit Pareek <rachitpareek@Rachits-MacBook-Pro.local>
2019-07-27 22:20:59 -0700

    third

--- yuuvis-hack/src/components/NavigationBar.js
+++ yuuvis-hack/src/components/NavigationBar.js
@@ -1,32 +1,53 @@
 import React, { Component } from 'react';
-import Navbar from 'react-bootstrap/Navbar'
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import favicon from '../favicon.svg';
+import '../App.css';
 
-export default class NavigationBar extends Component {
-    render() {
-        return (
-            <div>
-                <Navbar bg="light" expand="lg">
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                        <Form inline>
-                            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                            <Button variant="outline-success">Search</Button>
-                        </Form>
-                    </Navbar.Collapse>
-                </Navbar>
+export default function NavigationBar() {
+
+    return (
+
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <img src={favicon} alt="logo" height="2.5%" width="2.5%" />
+            <a class="navbar-brand ml-2" href="/">Smart Security Systems</a>
+
+            <div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarColor01">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/about">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/logIn">Log In </a>
+                    </li>
+                </ul>
             </div>
-        )
-    }
+
+        </nav>
+
+
+
+    )
 }
+
+
+// export default function NavigationBar() {
+
+//     return (
+//         <div>
+//             <Navbar bg="dark" fixed="top" >
+//                 <img src={favicon} alt="logo" height="3%" width="3%" />
+//                 <Navbar.Brand href="#home">Smart Security Systems</Navbar.Brand>
+//                 <Nav pullRight>
+//                     <Nav.Link href="/">Home</Nav.Link>
+//                     <Nav.Link href="/about">About</Nav.Link>
+//                 </Nav>
+//                 <Button variant="outline-success" className="nav navbar-nav"><Nav.Link href="/logIn"></Nav.Link>Bob</Button>
+//             </Navbar>
+//         </div>
+//     )
+// }
+
+
--- yuuvis-hack/src/pages/HomePage.js
+++ yuuvis-hack/src/pages/HomePage.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import logo from '../logo.svg';
+import favicon from '../favicon.svg';
 import NavigationBar from '../components/NavigationBar';
+
+const butStyle = {
+    margin: '20px',
+    width: '200px',
+    background: '#D3D3D3',
+    color: 'black'
+};
 
 function HomePage() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React!!!
-          </a>
-        </header>
-        <NavigationBar></NavigationBar>
-      </div>
+        <div className="App">
+            <NavigationBar></NavigationBar>
+            <body className="App-header">
+                <br></br>
+                <br></br>
+                <br></br>
+                <h1>Optimized security footage storage, analysis, and archival.</h1>
+                <br></br>
+                <img src={favicon} className="App-logo" alt="logo" />
+                <br></br>
+                <div>
+                    <a type="button" href="/about" class="btn btn-secondary btn-lg" style={butStyle}>Learn More</a>
+                    <a type="button" href="/signUp" class="btn btn-secondary btn-lg" style={butStyle}>Sign Up!</a>
+                </div>
+            </body>
+
+        </div>
     );
-  }
-  
-  export default HomePage;
-  
+}
+
+export default HomePage;
